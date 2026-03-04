45acc77a9d7a2760c97a3a659dd0c9d29c7f63a8
shaggyBadger <josh.ballard1983@gmail.com>
2026-03-04 02:18:25 -0500

    another bookmark

--- thejoshproject/settings.py
+++ thejoshproject/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "homepage",
 ]
 
 MIDDLEWARE = [
@@ -45,10 +46,11 @@ ROOT_URLCONF = "thejoshproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # Add this line
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",  # Add this line
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -113,3 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = (
+    BASE_DIR / "staticfiles"
+)  # Directory where static files will be collected for deployment
+STATICFILES_DIRS = [
+    BASE_DIR / "static_common",  # Our new project-level static directory
+]
