89fffc2cfc3ec441ed92774a828732122a2ce824
shaggyBadger <josh.ballard1983@gmail.com>
2026-04-12 02:32:22 -0400

    still messing with settings

--- thejoshproject/settings.py
+++ thejoshproject/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = (
-    BASE_DIR / "staticfiles"
-)  # Directory where static files will be collected for deployment
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    BASE_DIR / "static_common",  # Our new project-level static directory
+    BASE_DIR / "static_common",
 ]
