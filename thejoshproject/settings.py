fe4906875c46d36ad6d02af29033a179eb1d81c1
shaggyBadger <josh.ballard1983@gmail.com>
2026-04-12 02:05:54 -0400

    update settings so it has allowed hosts and debug=false

--- thejoshproject/settings.py
+++ thejoshproject/settings.py
@@ -14,9 +14,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "thejoshproject.xyz",
+    "www.thejoshproject.xyz",
+    "45.79.212.144",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
