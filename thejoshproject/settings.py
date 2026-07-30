43d7fa0431b8c136fc2589d8307e8cc64fa81c79
shaggyBadger <josh.ballard1983@gmail.com>
2026-07-30 13:45:32 -0400

    updated settings. idk why

--- thejoshproject/settings.py
+++ thejoshproject/settings.py
@@ -317,6 +317,19 @@ if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 APP_VERSION = "2.4.2"
 
+# Zoho SMTP for Tank Chart Email Delivery
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.zoho.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "josh@thejoshproject.xyz")
+EMAIL_HOST_PASSWORD = os.environ.get("ZOHO_EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.environ.get(
+    "DEFAULT_FROM_EMAIL",
+    "webNexus Tank Charts <josh@thejoshproject.xyz>",
+)
+EMAIL_TIMEOUT = 30
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
